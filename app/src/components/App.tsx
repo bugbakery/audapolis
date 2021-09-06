8d7e8345959073088829747535021e27a3099c2b
Jaro Habiger <jarohabiger@googlemail.com>
2021-09-06 10:58:48 +0200

    Add window with basic client sided decoration
    this still needs to be tested & adopted to other platforms tho

--- app/src/components/App.tsx
+++ app/src/components/App.tsx
@@ -1,5 +1,28 @@
 import * as React from 'react';
+import { LandingPage } from './Landing';
+import { TitleBar } from './TitleBar';
+import styled from 'styled-components';
+
+const AppContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+`;
+const MainContainer = styled.div`
+  overflow: auto;
+  align-self: stretch;
+  flex-grow: 1;
+  display: flex;
+`;
 
 export default function App(): JSX.Element {
-  return <div>Hello, world!</div>;
+  return (
+    <AppContainer>
+      <TitleBar />
+      <MainContainer>
+        <LandingPage />
+      </MainContainer>
+    </AppContainer>
+  );
 }

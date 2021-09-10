1cc4909678ee828fc408fe11d7d9704747bff0da
pajowu <git@ca.pajowu.de>
2021-09-10 13:10:17 +0200

    ✨ Server config, specify model in transcription endpoint

--- server/scripts/transcribe.py
+++ server/scripts/transcribe.py
@@ -1,18 +1,23 @@
 import argparse
-from pathlib import Path
-from pprint import pprint
 import json
 import zipfile
+from pathlib import Path
+from pprint import pprint
+
 import requests
 
 parser = argparse.ArgumentParser()
 parser.add_argument("file", type=Path)
+parser.add_argument("lang", type=str)
+parser.add_argument("model", type=str)
 parser.add_argument("--server", default="http://127.0.0.1:8000")
 args = parser.parse_args()
 
 print(f"Uploading {args.file}")
 upload_req = requests.post(
-    f"{args.server}/tasks/start_transcription/", files={"file": open(args.file, "rb")}
+    f"{args.server}/tasks/start_transcription/",
+    files={"file": open(args.file, "rb")},
+    params={"lang": args.lang, "model": args.model},
 )
 
 while True:
@@ -23,13 +28,13 @@ while True:
 
     print(status_req.json(), end="\r")
 
-output_file = args.file.with_suffix('.audapolis')
-content = status_req.json()['content']
+output_file = args.file.with_suffix(".audapolis")
+content = status_req.json()["content"]
 for paragraph in content:
-    for word in paragraph['content']:
-        word['source'] = 0
-        
-document = {"sources": [{"fileName": args.file.name}], "content":content}
+    for word in paragraph["content"]:
+        word["source"] = 0
+
+document = {"sources": [{"fileName": args.file.name}], "content": content}
 print()
 print(document)
 with zipfile.ZipFile(output_file, "w") as audapolis_zip:

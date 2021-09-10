8152713f1b2067415540cb85070c6b8e223d56d6
Jaro Habiger <jarohabiger@googlemail.com>
2021-09-10 15:52:45 +0200

    refactor: use thunkApi instread of store dispatch

--- app/src/state/transcribe.ts
+++ app/src/state/transcribe.ts
@@ -1,28 +1,30 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import { ipcRenderer } from 'electron';
-import { store } from './index';
 import { openTranscribe, openLanding } from './nav';
 
 export interface TranscribeState {
   file?: string;
 }
 
-export const transcribeFile = createAsyncThunk('transcribe/transcribeFile', async () => {
-  const file = await ipcRenderer.invoke('open-file', {
-    properties: ['openFile'],
-    promptToCreate: true,
-    createDirectory: true,
-    filters: [
-      { name: 'Audio Files', extensions: ['wav', 'mp3'] },
-      { name: 'All Files', extensions: ['*'] },
-    ],
-  });
-  store.dispatch(openTranscribe());
-  return file.filePaths[0];
-});
+export const transcribeFile = createAsyncThunk(
+  'transcribe/transcribeFile',
+  async (_, { dispatch }) => {
+    const file = await ipcRenderer.invoke('open-file', {
+      properties: ['openFile'],
+      promptToCreate: true,
+      createDirectory: true,
+      filters: [
+        { name: 'Audio Files', extensions: ['wav', 'mp3'] },
+        { name: 'All Files', extensions: ['*'] },
+      ],
+    });
+    dispatch(openTranscribe());
+    return file.filePaths[0];
+  }
+);
 
-export const abortTranscription = createAsyncThunk('transcribe/abort', async () => {
-  store.dispatch(openLanding());
+export const abortTranscription = createAsyncThunk('transcribe/abort', async (_, { dispatch }) => {
+  dispatch(openLanding());
 });
 
 export const importSlice = createSlice({

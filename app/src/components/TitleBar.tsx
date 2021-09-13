cbfa6df78ba2cfced22605021f01524b4eca253f
Jaro Habiger <jarohabiger@googlemail.com>
2021-09-13 10:08:46 +0200

    fix theme in Titlebar

--- app/src/components/TitleBar.tsx
+++ app/src/components/TitleBar.tsx
@@ -91,6 +91,7 @@ const TitleBarButtonContainer = styled.button<{ clicked?: boolean }>`
   & > * {
     width: 100%;
     height: 100%;
+    filter: var(--filter);
   }
 `;
 export function TitleBarButton(

3b5b33b05f1728f56098d871113946d9a1df42c3
Jaro Habiger <jarohabiger@googlemail.com>
2021-09-14 14:18:37 +0200

    refactor: move assertSome to util

--- app/src/state/util.ts
+++ app/src/state/util.ts
@@ -1,3 +1,9 @@
 export function sleep(time: number): Promise<void> {
   return new Promise((resolve) => setTimeout(resolve, time));
 }
+
+export function assertSome<T>(x: T | null | undefined | void): asserts x is T {
+  if (x === undefined || x === null) {
+    throw Error(`x is not loaded (it is '${x}')`);
+  }
+}

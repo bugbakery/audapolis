8d8ac9052cb6e4d0a124fd06d249c675b55bbaf5
Jaro Habiger <jarohabiger@googlemail.com>
2021-09-17 15:37:11 +0200

    add ui for selecting the transcription model

--- app/src/components/Transcribe.tsx
+++ app/src/components/Transcribe.tsx
@@ -6,27 +6,52 @@ import { TitleBar } from './TitleBar';
 import { AppContainer, MainCenterColumn } from './Util';
 import { RootState } from '../state';
 import styled from 'styled-components';
+import { openSettings } from '../state/nav';
+import { useState } from 'react';
 
-const FormRow = styled.div`
-  display: flex;
-  flex-direction: row;
-  padding: 10px;
-  font-size: 18px;
+const Form = styled.div`
+  padding: 20px;
+  margin-bottom: 50px;
+  display: grid;
+  grid-template-columns: auto auto;
+  grid-gap: 20px;
 `;
 
 export function TranscribePage(): JSX.Element {
   const dispatch = useDispatch();
   const file = useSelector((state: RootState) => state.transcribe.file) || '';
+  const models = useSelector((state: RootState) =>
+    Object.values(state.models.downloaded).flatMap((x) => x)
+  );
+
+  const [selectedModel, setSelectedModel] = useState(0);
 
   return (
     <AppContainer>
       <TitleBar />
       <MainCenterColumn>
-        <FormRow>
-          <p style={{ opacity: 0.5 }}>opened</p>&nbsp;
-          <p> {file.split('/').pop()}</p>
-        </FormRow>
-        <Button primary onClick={() => dispatch(startTranscription())}>
+        <Form>
+          <span style={{ opacity: 0.5 }}>opened</span>
+          <span> {file.split('/').pop()}</span>
+
+          <span style={{ opacity: 0.5 }}>Transcription Model</span>
+          <select
+            value={selectedModel}
+            onChange={(e) => setSelectedModel(parseInt(e.target.value))}
+          >
+            {models.map((model, i) => (
+              <option key={i} value={i}>
+                {model.lang} - {model.name}
+              </option>
+            ))}
+          </select>
+
+          <a style={{ gridColumn: '1 / 3' }} onClick={() => dispatch(openSettings())}>
+            Download More Transcription Models
+          </a>
+        </Form>
+
+        <Button primary onClick={() => dispatch(startTranscription(models[selectedModel]))}>
           Start Transcribing
         </Button>
         <Button onClick={() => dispatch(abortTranscription())}>abort</Button>

78859fe230f8eed484fa2bf28b86f19385cc5c61
Jaro Habiger <jarohabiger@googlemail.com>
2021-10-18 14:14:34 +0200

    :see_no_evil: Fix file open dialog bug

--- app/src/main_process/ipc.ts
+++ app/src/main_process/ipc.ts
@@ -4,11 +4,11 @@ import { assertSome } from '../util';
 ipcMain.handle('open-file', (event, options) => {
   const win = BrowserWindow.fromWebContents(event.sender);
   assertSome(win);
-  return dialog.showOpenDialogSync(win, options);
+  return dialog.showOpenDialog(win, options);
 });
 
 ipcMain.handle('save-file', (event, options) => {
   const win = BrowserWindow.fromWebContents(event.sender);
   assertSome(win);
-  return dialog.showSaveDialogSync(win, options);
+  return dialog.showSaveDialog(win, options);
 });

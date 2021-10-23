fc4f1b14b5aa92ec9e148e2634cb2b2d8db8767f
Jaro Habiger <jarohabiger@googlemail.com>
2021-10-23 23:07:39 +0200

    :rotating_light: fix eslint warnings

--- app/src/main_process/windowList.ts
+++ app/src/main_process/windowList.ts
@@ -2,7 +2,7 @@ import { BrowserWindow } from 'electron';
 
 export const windowList: BrowserWindow[] = [];
 
-export function sendAll(channel: string, payload: any): void {
+export function sendAll(channel: string, payload: unknown): void {
   windowList.forEach((window) => {
     window.webContents.send(channel, payload);
   });

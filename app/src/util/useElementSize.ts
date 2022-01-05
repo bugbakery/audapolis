1fa1d446e1d19d9570f874c194c72a01f911cf9d
Jaro Habiger <jarohabiger@googlemail.com>
2022-01-05 18:26:40 +0100

    fix selectionMenu positioning on document resize

--- app/src/util/useElementSize.ts
+++ app/src/util/useElementSize.ts
@@ -1,7 +1,8 @@
 import { useEffect, useState } from 'react';
 
 export function useElementSize(
-  element: HTMLElement | undefined | null
+  element: HTMLElement | undefined | null,
+  callback?: (size: { width: number; height: number }) => void
 ): { width: number; height: number } | null {
   const [size, setSize] = useState(null as null | { height: number; width: number });
   useEffect(() => {
@@ -11,14 +12,15 @@ export function useElementSize(
       const rect = element.getClientRects().item(0);
       if (!rect) return;
       const { width, height } = rect;
-      setSize({ height, width });
+      if (callback) callback({ width, height });
+      setSize({ width, height });
     });
     observer.observe(element);
 
     return () => {
       observer.disconnect();
     };
-  }, [element]);
+  }, [element, callback]);
 
   return size;
 }

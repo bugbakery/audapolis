a8e3b84d7028dd80441163e9bcceeaadb8c571ea
pajowu <git@ca.pajowu.de>
2022-01-07 00:06:08 +0100

    ✨ Export to WebVTT

--- app/src/pages/Editor/ExportDocumentDialog.tsx
+++ app/src/pages/Editor/ExportDocumentDialog.tsx
@@ -1,43 +1,28 @@
-import React, { ChangeEvent, useState } from 'react';
+import React, { MutableRefObject, useRef, useState } from 'react';
 import { RootState } from '../../state';
 import { useDispatch, useSelector, useStore } from 'react-redux';
 import { setExportPopup } from '../../state/editor';
 import path from 'path';
 import { FilePickerWithText } from '../../components/FilePicker';
 import { FileFilter } from 'electron';
-import { player } from '../../core/player';
 import { toast } from 'react-hot-toast';
-import { exportAudio, exportVideo } from '../../core/ffmpeg';
-import { DocumentGenerator } from '../../core/document';
+import { Document } from '../../core/document';
 import { assertSome } from '../../util';
-import { exportOtio } from '../../core/otio';
-import {
-  Button,
-  Combobox,
-  CrossIcon,
-  Dialog,
-  FormField,
-  Group,
-  IconButton,
-  majorScale,
-  TextInput,
-} from 'evergreen-ui';
+import { Button, Combobox, Dialog, FormField, majorScale } from 'evergreen-ui';
+import { Video } from './ExportOptions/Video';
+import { Otio } from './ExportOptions/Otio';
+import { Audio } from './ExportOptions/Audio';
+import { WebVtt } from './ExportOptions/WebVtt';
 
-interface audioExport {
-  type: 'audio';
+type ExportType = {
+  type: string;
   path: string;
-}
-interface videoExport {
-  type: 'video';
-  path: string;
-  width: number;
-  height: number;
-}
-interface finalcutExport {
-  type: 'OpenTimelineIO';
-  path: string;
-}
-type ExportType = audioExport | videoExport | finalcutExport;
+  filters: FileFilter[];
+  // We pass a ref as onExport which the function will set to its export function
+  component: (props: {
+    exportCallbackRef: MutableRefObject<(document: Document, path: string) => Promise<void>>;
+  }) => JSX.Element;
+};
 
 export function ExportDocumentDialog(): JSX.Element {
   const dispatch = useDispatch();
@@ -45,65 +30,58 @@ export function ExportDocumentDialog(): JSX.Element {
   const documentPath = useSelector((state: RootState) => state.editor.present?.path || '');
   const documentBaseName = path.basename(documentPath, '.audapolis');
   const documentBasePath = path.join(path.dirname(documentPath), documentBaseName);
-  const targetResolution = player.getTargetResolution();
+  const exportFnRef = useRef((_a: Document, _b: string) => {
+    return new Promise<void>((resolve, _) => {
+      resolve();
+    });
+  });
   const exportValues: (ExportType & { filters: FileFilter[] })[] = [
     {
       type: 'audio',
       path: documentBasePath + '.mp3',
       filters: [{ name: 'Audio Files', extensions: ['mp3', 'wav', 'ogg', 'wma', 'aac'] }],
+      component: Audio,
     },
     {
       type: 'video',
       path: documentBasePath + '.mp4',
-      width: targetResolution.x,
-      height: targetResolution.y,
       filters: [{ name: 'Video Files', extensions: ['mp4', 'mkv', 'gif'] }],
+      component: Video,
     },
     {
       type: 'OpenTimelineIO',
       path: documentBasePath + '_proj',
       filters: [],
-    },
-  ];
-
-  const otioFormats = [
-    {
-      name: 'Final Cut Pro',
-      adapter: 'fcp_xml',
-      extension: 'xml',
+      component: Otio,
     },
     {
-      name: 'Final Cut Pro X',
-      adapter: 'fcpx_xml',
-      extension: 'xml',
-    },
-    {
-      name: 'Avid AAF',
-      adapter: 'aaf',
-      extension: 'aaf',
-    },
-    {
-      name: 'Pitivi (Gstreamer Editing Services)',
-      adapter: 'xges',
-      extension: 'xges',
-    },
-    {
-      name: 'Kdenlive',
-      adapter: 'kdenlive',
-      extension: 'kdenlive',
-    },
-    {
-      name: 'OpenTimelineIO JSON',
-      adapter: 'otio_json',
-      extension: 'json',
+      type: 'WebVTT',
+      path: documentBasePath + '.vtt',
+      filters: [{ name: 'WebVTT Files', extensions: ['vtt'] }],
+      component: WebVtt,
     },
   ];
 
   const [formState, setFormState] = useState(exportValues[0]);
-  const [otioFormat, setOtioFormat] = useState(otioFormats[0]);
 
   const popupState = useSelector((state: RootState) => state.editor.present?.exportPopup);
+  const exportFn = (close: () => void) => {
+    const action = async () => {
+      close();
+      const state: RootState = store.getState();
+      assertSome(state.editor.present);
+      await exportFnRef.current(state.editor.present.document, formState.path);
+    };
 
+    toast
+      .promise(action(), {
+        loading: 'Exporting...',
+        success: <b>Export successful!</b>,
+        error: <b>Export failed.</b>,
+      })
+      .catch((e) => console.log(e));
+  };
+  const ExportOptionComponent = formState.component;
   return (
     <Dialog
       onCloseComplete={() => dispatch(setExportPopup(false))}
@@ -112,55 +90,7 @@ export function ExportDocumentDialog(): JSX.Element {
       footer={({ close }) => (
         <>
           <Button onClick={() => dispatch(setExportPopup(false))}>abort</Button>
-          <Button
-            marginLeft={majorScale(1)}
-            appearance={'primary'}
-            onClick={() => {
-              const action = async () => {
-                close();
-
-                const state: RootState = store.getState();
-                assertSome(state.editor.present);
-
-                const renderItems = DocumentGenerator.fromParagraphs(
-                  state.editor.present.document.content
-                )
-                  .toRenderItems()
-                  .collect();
-                const sources = state.editor.present.document.sources;
-
-                if (formState.type == 'audio') {
-                  await exportAudio(renderItems, sources, formState.path);
-                } else if (formState.type == 'video') {
-                  await exportVideo(renderItems, sources, formState.path, {
-                    x: formState.width,
-                    y: formState.height,
-                  });
-                } else if (formState.type == 'OpenTimelineIO') {
-                  const server = state.server.servers[state.server.selectedServer];
-                  await exportOtio(
-                    documentBaseName,
-                    otioFormat.extension,
-                    otioFormat.adapter,
-                    renderItems,
-                    sources,
-                    formState.path,
-                    server
-                  );
-                } else {
-                  throw Error('dont know how to export that :(');
-                }
-              };
-
-              toast
-                .promise(action(), {
-                  loading: 'Exporting...',
-                  success: <b>Export successful!</b>,
-                  error: <b>Export failed.</b>,
-                })
-                .catch((e) => console.log(e));
-            }}
-          >
+          <Button marginLeft={majorScale(1)} appearance={'primary'} onClick={() => exportFn(close)}>
             Start Export
           </Button>
         </>
@@ -184,49 +114,7 @@ export function ExportDocumentDialog(): JSX.Element {
         />
       </FormField>
 
-      {formState.type == 'video' ? (
-        <FormField label="Resolution" marginBottom={majorScale(3)}>
-          {/*<div style={{ display: 'flex', flexDirection: 'row' }}>*/}
-          <Group width={'100%'}>
-            <TextInput
-              value={formState.width}
-              onChange={(e: ChangeEvent<HTMLInputElement>) =>
-                setFormState((last) => ({ ...last, width: parseInt(e.target.value) }))
-              }
-              flex={1}
-              type={'number'}
-            />
-            <IconButton icon={CrossIcon} appearance={'minimal'} disabled={true}></IconButton>
-            <TextInput
-              value={formState.height}
-              onChange={(e: ChangeEvent<HTMLInputElement>) =>
-                setFormState((last) => ({ ...last, height: parseInt(e.target.value) }))
-              }
-              flex={1}
-              type={'number'}
-            />
-          </Group>
-          {/*</div>*/}
-        </FormField>
-      ) : (
-        <></>
-      )}
-
-      {formState.type == 'OpenTimelineIO' ? (
-        <>
-          <FormField label="Output Format" marginBottom={majorScale(3)}>
-            <Combobox
-              width={'100%'}
-              initialSelectedItem={otioFormat}
-              items={otioFormats}
-              itemToString={(x) => x.name}
-              onChange={(selected) => setOtioFormat(selected)}
-            />
-          </FormField>
-        </>
-      ) : (
-        <></>
-      )}
+      <ExportOptionComponent exportCallbackRef={exportFnRef} />
     </Dialog>
   );
 }

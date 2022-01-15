7092081e36a526b4088de7ae700f616291906977
Jaro Habiger <jarohabiger@googlemail.com>
2022-01-15 01:35:53 +0100

    ♻️ change api for create{Async,}ActionWithReducer
    this should improve testability as now the fulfilled, rejected and pending reducer-functions of createActionWithReducer are fields with stable names and thus available for testing.

--- app/src/state/util.ts
+++ app/src/state/util.ts
@@ -1,22 +1,34 @@
 import { Draft } from 'immer';
-import { AsyncThunk, createAsyncThunk } from '@reduxjs/toolkit';
+import { AnyAction, AsyncThunk, createAsyncThunk } from '@reduxjs/toolkit';
 import { AsyncThunkPayloadCreator } from '@reduxjs/toolkit/dist/createAsyncThunk';
 import { RootState } from './index';
 
 type ReducerType<StateSlice, Payload = void> = (state: Draft<StateSlice>, payload: Payload) => void;
 
-type PayloadActionCreator<Payload> = (payload: Payload) => { type: string; payload: Payload };
-export type ActionWithReducers<StateSlice, Payload> = PayloadActionCreator<Payload> & {
-  reducer: ReducerType<StateSlice, Payload>;
-  type: string;
+// for consumption by the top level reducer
+type ReducerApi<StateSlice> = {
+  handleAction: ReducerType<StateSlice, AnyAction>;
 };
+
+type PayloadActionCreator<Payload> = (payload: Payload) => { type: string; payload: Payload };
+export type ActionWithReducers<StateSlice, Payload> = PayloadActionCreator<Payload> &
+  ReducerApi<StateSlice> & {
+    reducer: ReducerType<StateSlice, Payload>;
+    type: string;
+  };
+
 export type AsyncActionWithReducers<StateSlice, Returned, ThunkArg> = AsyncThunk<
   Returned,
   ThunkArg,
   Record<string, never>
-> & {
-  reducers: { [x: string]: ReducerType<StateSlice, Returned | Error | void> };
-};
+> &
+  ReducerApi<StateSlice> & {
+    reducers?: {
+      pending?: ReducerType<StateSlice, void>;
+      rejected?: ReducerType<StateSlice, Error>;
+      fulfilled?: ReducerType<StateSlice, Returned>;
+    };
+  };
 
 /**
  * This function creates a special type that is both an action creator and contains the corresponding reducer for these actions.
@@ -35,36 +47,46 @@ export function createActionWithReducer<StateSlice, Payload = void>(
   reducer: ReducerType<StateSlice, Payload>
 ): ActionWithReducers<StateSlice, Payload> {
   const actionCreator: PayloadActionCreator<Payload> = (payload) => ({ type, payload });
-  return Object.assign(actionCreator, { reducer, type });
+  const handleAction: ReducerType<StateSlice, AnyAction> = (state, action) => {
+    if (action.type == type) reducer(state, action.payload);
+  };
+  return Object.assign(actionCreator, { reducer, handleAction, type });
 }
 
 /**
  * this function works analogous to the way the `createActionReducerPair` function works. the difference is that this
  * function also gets an async thunk function as a argument, so it behaves like an async thunk. the
- * `thunk.rejected`, `thunk.fulfilled`, and `thunk.pending` actions can be handled in the reducer object and are thus
+ * `thunk.rejected`, `thunk.fulfilled`, and `thunk.pending` actions can be handled in the reducers object and are thus
  * called after the thunk code.
  * Beware: Even if you theoretically can dispatch other actions and get the full state in your thunk code, this is a
  * potential foot-gun as you could violate atomicity guarantees in a non-obvious way.
  *
  * @param type the string uniquely identifying the action
  * @param payloadCreator the async function creating the action
- * @param reducer the reducer associated with this action
+ * @param reducers the reducers associated with this action
  */
 export function createAsyncActionWithReducer<StateSlice, ThunkArg = void, Returned = void>(
   type: string,
   payloadCreator: AsyncThunkPayloadCreator<Returned, ThunkArg, { state: RootState }>,
-  reducer?: {
+  reducers?: {
     pending?: ReducerType<StateSlice>;
-    rejected?: (state: Draft<StateSlice>, error: Error) => void;
+    rejected?: ReducerType<StateSlice, Error>;
     fulfilled?: ReducerType<StateSlice, Returned>;
   }
 ): AsyncActionWithReducers<StateSlice, Returned, ThunkArg> {
   const thunk = createAsyncThunk<Returned, ThunkArg>(type, payloadCreator);
-  const reducers: { [x: string]: ReducerType<StateSlice, Returned | Error | void> } = {};
 
-  if (reducer?.pending) reducers[thunk.pending.type] = reducer.pending;
-  if (reducer?.rejected) reducers[thunk.rejected.type] = reducer.rejected;
-  if (reducer?.fulfilled) reducers[thunk.fulfilled.type] = reducer.fulfilled;
+  const handleAction: ReducerType<StateSlice, AnyAction> = (state, action) => {
+    if (reducers) {
+      if (reducers.pending && action.type == thunk.pending.type) {
+        reducers.pending(state);
+      } else if (reducers.rejected && action.type == thunk.rejected.type) {
+        reducers.rejected(state, action.payload);
+      } else if (reducers.fulfilled && action.type == thunk.fulfilled.type) {
+        reducers.fulfilled(state, action.payload);
+      }
+    }
+  };
 
-  return Object.assign(thunk, { reducers });
+  return Object.assign(thunk, { reducers, handleAction });
 }

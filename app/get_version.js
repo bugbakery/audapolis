817ff7935f60352baa21ab94560b32f575778c3d
Karl <pajowu@users.noreply.github.com>
2022-02-17 18:45:48 +0100

    💚  Use - instead of + in release version

--- app/get_version.js
+++ app/get_version.js
@@ -8,16 +8,16 @@ function getVersion() {
     cwd: __dirname,
     encoding: 'utf-8',
   };
-  const lastBumpCommit = child_process.execSync(
-    `git log --no-patch --pretty="%H" -L /version/,+1:package.json`,
-    execOptions
-  );
+  const lastBumpCommit = child_process
+    .execSync(`git log --no-patch --pretty="%H" -L /version/,+1:package.json`, execOptions)
+    .trim()
+    .split('\n')[0];
   const commitListSinceBump = child_process.execSync(
     `git rev-list ${lastBumpCommit.trim()}..HEAD`,
     execOptions
   );
   const noCommitsSinceBump = commitListSinceBump.trim().split('\n').length;
-  return `${baseVersion}+${noCommitsSinceBump}`;
+  return `${baseVersion}-${noCommitsSinceBump}`;
 }
 
 module.exports = getVersion;

148d1ba8711b54ce88796f12c9a94a8c618bba4c
pajowu <git@ca.pajowu.de>
2023-07-07 13:31:28 +0200

    💚 Check that codesigning certificate was successfully imported

--- .github/workflows/import_cert.sh
+++ .github/workflows/import_cert.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 echo "Creating new keychain"
 
-security create-keychain -p "$MAC_CERTS_PASSWORD" build.keychain
+security create-keychain -p "$MAC_KEY_PASSWORD" build.keychain
 security default-keychain -s build.keychain
-security unlock-keychain -p "$MAC_CERTS_PASSWORD" build.keychain
+security unlock-keychain -p "$MAC_KEY_PASSWORD" build.keychain
 
 echo "Importing key"
-security import <(echo $MAC_KEY | base64 -d) -P "$MAC_CERTS_PASSWORD" -f pkcs12
+security import <(echo $MAC_KEY | base64 -d) -P "$MAC_KEY_PASSWORD" -f pkcs12
 
 echo "Trusting Certificate"
 # Using sudo because overwriting trust settings would require interactive password input
@@ -14,3 +14,11 @@ echo $MAC_CERT | base64 -d > cert.cer
 sudo security authorizationdb write com.apple.trust-settings.admin allow
 sudo security add-trusted-cert -d -r trustRoot -p codeSign -k build.keychain cert.cer
 sudo security authorizationdb remove com.apple.trust-settings.admin
+
+n_valid_certs=`security find-identity -v -p codesigning | grep 'valid identities found' | awk '{ print $1 }'`
+
+if [ $n_valid_certs -lt 1 ]; then
+    echo "Error, no valid codesigning certificate found"
+    security find-identity -p codesigning
+    exit 1
+fi
